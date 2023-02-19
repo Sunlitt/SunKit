6eed286094d7b618d1211b7801a3df145dbc2053
Davideilmito <81416309+davideilmito@users.noreply.github.com>
2023-02-19 14:09:21 +0100

    Added license note to each file

--- Sources/SunKit/Angle.swift
+++ Sources/SunKit/Angle.swift
@@ -1,9 +1,20 @@
 //
 //  Angle.swift
-//  
 //
-//  Created by Davide Biancardi on 25/11/22.
+//  Copyright 2023 Leonardo Bertinelli, Davide Biancardi, Raffaele Fulgente, Clelia Iovine, Nicolas Mariniello, Fabio Pizzano
 //
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 
 import Foundation
 
