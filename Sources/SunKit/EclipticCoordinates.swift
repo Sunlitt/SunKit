3f4c31386f4089a8380cf723ab88304916ac6a7f
Nicolas Mariniello <nmariniello.dev@icloud.com>
2024-02-09 17:08:04 +0100

    Updated license + added Settings.bundle example

--- Sources/SunKit/EclipticCoordinates.swift
+++ Sources/SunKit/EclipticCoordinates.swift
@@ -2,7 +2,7 @@
 //  EclipticCoordinates.swift
 //
 //
-//  Copyright 2023 Leonardo Bertinelli, Davide Biancardi, Raffaele Fulgente, Clelia Iovine, Nicolas Mariniello, Fabio Pizzano
+//   Copyright 2024 Leonardo Bertinelli, Davide Biancardi, Raffaele Fulgente, Clelia Iovine, Nicolas Mariniello, Fabio Pizzano
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
