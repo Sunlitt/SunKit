7ed9e3f5214fa632e1e115d81de9b51dc3bf7643
Nicolas Mariniello <nmariniello.dev@icloud.com>
2024-06-23 11:18:34 +0200

    Improved configuration

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SunKit",
     platforms: [
-        .iOS(.v14), .macOS(.v10_15), .tvOS(.v9), .watchOS(.v6)
+        .iOS(.v14), .macOS(.v10_15), .tvOS(.v12), .watchOS(.v6)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
